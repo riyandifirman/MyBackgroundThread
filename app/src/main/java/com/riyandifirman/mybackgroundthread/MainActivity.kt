c9b15ceaf7b89e2d9e94d282d6ebfd40ede404f4
riyandifirman <riyandi.firman08@gmail.com>
2023-03-16 04:10:21 +0700

    change executor and handler to coroutines

--- app/src/main/java/com/riyandifirman/mybackgroundthread/MainActivity.kt
+++ app/src/main/java/com/riyandifirman/mybackgroundthread/MainActivity.kt
@@ -6,6 +6,11 @@ import android.os.Handler
 import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
@@ -20,24 +25,40 @@ class MainActivity : AppCompatActivity() {
         val handler = Handler(Looper.getMainLooper())
 
         btnStart.setOnClickListener {
-            executor.execute {
-                try {
-                    for (i in 0..10) {
-                        Thread.sleep(1000)
-                        val percentage = i * 10
-                        handler.post {
-                            // update ui in main thread
-                            if (percentage == 100) {
-                                tvStatus.setText(R.string.task_completed)
-                            } else {
-                                tvStatus.text = String.format(getString(R.string.compressing), percentage)
-                            }
+// Using Coroutines
+            lifecycleScope.launch(Dispatchers.Default) {
+                for (i in 0..10) {
+                    delay(500)
+                    val percentage = i * 10
+                    withContext(Dispatchers.Main) {
+                        // update ui in main thread
+                        if (percentage == 100) {
+                            tvStatus.setText(R.string.task_completed)
+                        } else {
+                            tvStatus.text = String.format(getString(R.string.compressing), percentage)
                         }
                     }
-                } catch (e: InterruptedException) {
-                    e.printStackTrace()
                 }
             }
+// Using Handler and Executor
+//            executor.execute {
+//                try {
+//                    for (i in 0..10) {
+//                        Thread.sleep(1000)
+//                        val percentage = i * 10
+//                        handler.post {
+//                            // update ui in main thread
+//                            if (percentage == 100) {
+//                                tvStatus.setText(R.string.task_completed)
+//                            } else {
+//                                tvStatus.text = String.format(getString(R.string.compressing), percentage)
+//                            }
+//                        }
+//                    }
+//                } catch (e: InterruptedException) {
+//                    e.printStackTrace()
+//                }
+//            }
         }
     }
 }
